576be847917dd565db78ff5a50e87e9098820672
KoT3isGood <bratelllo@gmail.com>
2025-01-28 17:03:18 +0200

    fixed win32 surface

--- windows/window.c
+++ windows/window.c
@@ -93,7 +93,7 @@ window_handle_t createwindow() {
 window_handle_t createswapchain(window_handle_t handle) {
   {
     VkWin32SurfaceCreateInfoKHR createInfo = {};
-    createInfo.sType = VK_STRUCTURE_TYPE_XLIB_SURFACE_CREATE_INFO_KHR;
+    createInfo.sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
     createInfo.hwnd = handle.window;
     VkResult r = vkCreateWin32SurfaceKHR(instance,&createInfo,0,&handle.surface);  
     VK_PRINTRES("vkCreateWin32SurfaceKHR",r);
